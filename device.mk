841e8a29512fd959ab159e9b123d2157c3ffe46c
Rashed Abdel-Tawab <rashed@linux.com>
2015-11-01 21:25:19 -0500

    Update to OpenGLES 3.1
    Android 5.0 and forward support OpenGLES 3.1, but for some reason
    no one had implemented it yet. Might as well start now
    
    Change-Id: Ieee4697dafd9e67f399db7e9e6d20dd91c58be3a

--- device.mk
+++ device.mk
@@ -276,6 +276,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
