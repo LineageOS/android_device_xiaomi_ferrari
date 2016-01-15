6324b9e1288de10b0adbe3742118197c55924adc
Diogo Ferreira <defer@cyngn.com>
2016-01-15 18:02:15 +0000

    ferrari: Default to DDS flexmap triggering
    This changes the default so that the AOSP behavior prevails over
    qualcomm-specific implementations that use telephony-fwk.
    
    This change explicitly triggers a request to update modem capabilities
    when the data sub is changed thus allowing the second subscription
    to go over 2G, which wasn't possible previously.
    
    Change-Id: I20dd586ea74af7ba8ef6a1ce1cdd26c07d33d844

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -2,6 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    persist.radio.flexmap_type=dds \
     ro.telephony.default_network=9,1
 
 # RIL
