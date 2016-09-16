0186e31df9e666d5fc7c50b0ff9401818b77ef9a
Michael Gernoth <michael@gernoth.net>
2016-09-16 20:33:00 +0530

    ferrari: ril: Return static radio capability from getRadioCapability
    * Our modem somehow returns incorrect radio
      capabilities for 2nd slot, that makes it
      stay on EDGE.
    * Change default network on 2nd slot to LTE
    Change-Id: I7b490d39304a03aac55c86c44a5af86c6a6dea72

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -2,7 +2,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-    ro.telephony.default_network=9,1
+    ro.telephony.ril_class=XiaomiRIL \
+    ro.telephony.default_network=9,9
 
 # RIL
 ifeq ($(QCPATH),)
