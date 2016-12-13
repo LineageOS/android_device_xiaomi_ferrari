b94ce6c611f5d03f935b45a5d07c2377c7a97802
rohan <purohit.rohan@gmail.com>
2016-12-13 11:27:17 +0530

    ferrari: use device specific ueventd.rc
    * fixes sensors

--- product/init.mk
+++ product/init.mk
@@ -3,3 +3,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.fm.sh \
     init.target.rc
+
+# Temp, until we find a better way
+PRODUCT_COPY_FILES += \
+    device/xiaomi/ferrari/rootdir/etc/ueventd.qcom.rc:root/ueventd.qcom.rc
