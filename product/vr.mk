0fcdef1b54950edffa962892578ad1c2c90a0a77
Haikal Izzuddin <heywhite69@gmail.com>
2016-12-15 17:04:13 +0800

    ferrari: Add support for VR
    Change-Id: Ied235946ae69a532e2f0a05f74fc59b9512fc5b8

---
+++ product/vr.mk
@@ -0,0 +1,6 @@
+# VR HAL
+PRODUCT_PACKAGES += \
+    vr.ferrari
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
