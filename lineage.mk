7cf8f9253bd51924569e2d383b8d3bd40fad485c
rohan <purohit.rohan@gmail.com>
2017-01-01 20:47:49 +0530

    ferrari: Rebrand to LineageOS
    Change-Id: Iece73fdb6033d66c5cfc7f8a7a104b4e4f5e9620

--- lineage.mk
+++ lineage.mk
@@ -14,7 +14,7 @@
 
 $(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
@@ -22,7 +22,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ferrari
-PRODUCT_NAME := cm_ferrari
+PRODUCT_NAME := lineage_ferrari
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 4i
 PRODUCT_MANUFACTURER := Xiaomi
