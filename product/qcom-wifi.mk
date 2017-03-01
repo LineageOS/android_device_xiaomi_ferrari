b953dac874408d1f4a653cd80178f54c6bc6dc90
Louis Popi <theh2o64@gmail.com>
2017-03-01 07:51:14 +0100

    ferrari: Use commonized WCNSS config
    Change-Id: I06ec9451fbae616a7e2b83a14ae679f323508dc0

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -5,5 +5,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
