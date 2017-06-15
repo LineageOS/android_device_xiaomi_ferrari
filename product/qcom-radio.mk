04d33b6e7753c668e23b1a074b51b02777bf2d3b
thewisenerd <thewisenerd@protonmail.com>
2017-06-15 22:03:45 +0530

    ferrari: update proprietary-files
     - update DRM blobs (from crackling)
     - update Widevine blobs (from kipper)
     - update graphics blobs (from crackling)
     - update perfd (from kipper)
     - update radio (from kipper)
     - update qcom fw (from kipper)
     - remove unused graphics blobs
     - remove prebuilt audio hal
     - comment out files we either have in device tree, or do not have
     - update script templates from upstream
     - rearrange in a meaningful manner (hopelessly)
    
    Change-Id: Ieba4a1e65b53fb0bb5890073249bd1bcceb7318a

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -3,11 +3,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ro.telephony.default_network=9,1
-
-# RIL
-ifeq ($(QCPATH),)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
-    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
-    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
-endif
