5f9d3e27b95454c19c6228e0a24c992e5c7176c3
thewisenerd <thewisenerd@protonmail.com>
2017-06-18 22:10:29 +0530

    ferrari: remove init.qcom.fm.sh
    this is not needed;
    
    Change-Id: I357524bc5c5733e48e2bd4129599e4dfcb88d831

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,13 +3,6 @@ include $(CLEAR_VARS)
 
 # QCOM config scripts
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.fm.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
-include $(BUILD_PREBUILT)
-
 # Device init files
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
